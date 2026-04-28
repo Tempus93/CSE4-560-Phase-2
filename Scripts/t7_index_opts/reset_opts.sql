d1239cb791f9806cc708a8d38de0de5cad18ecf9
Colin <129790765+korinX@users.noreply.github.com>
2026-04-28 11:19:07 -0400

    task 7

---
+++ Scripts/t7_index_opts/reset_opts.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_snapcounts_player_game;
+DROP INDEX IF EXISTS idx_snapcounts_percentage;
+DROP INDEX IF EXISTS idx_player_position;
+DROP INDEX IF EXISTS idx_games_seasonid;
+DROP INDEX IF EXISTS idx_player_teamid;
+DROP INDEX IF EXISTS idx_playerstats_player_game;
+DROP INDEX IF EXISTS idx_playerstats_offstatid;
